be53ce6213c21eca18068b03a3e6fda4adae76ff
Suhail-22 <101893675+Suhail-22@users.noreply.github.com>
2025-11-30 00:08:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,51 +1,20 @@
-const CACHE_NAME = 'asc-calc-final-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/index.tsx',
-  '/manifest.json',
-  '/assets/icon-192-192.png',
-  '/assets/icon-512-512.png'
+const CACHE_NAME = 'asc-cache-v3';
+const URLS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json'
 ];
 
-// تثبيت Service Worker
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(URLS_TO_CACHE))
+    );
 });
 
-// تفعيل Service Worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// fetch الأحداث
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
